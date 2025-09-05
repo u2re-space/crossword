75279af8e510bd3efde1e34c58fc33b8a60e851f
u2re-dev <c24b@u2re.ru>
2025-09-05 22:19:00 +0700

    API in more correct format...

--- src/core/api/endpoints/GPT-Conversion.ts
+++ src/core/api/endpoints/GPT-Conversion.ts
@@ -4,18 +4,29 @@ import { DataInput, DataKind, getDataKindByMIMEType, typesForKind } from "./GPT-
 export const getUsableData = async (data: DataInput) => {
     if (data?.dataSource instanceof Blob || data?.dataSource instanceof File) {
         if (data?.dataKind === "image") {
-            const BASE64URL = `data:${data?.dataSource?.type};base64,`; // @ts-ignore
-            return BASE64URL+(new Uint8Array(await data?.dataSource?.arrayBuffer())?.toBase64?.({ alphabet: "base64url" }));
+            const BASE64URL = `data:${data?.dataSource?.type};base64,`;
+            return {
+                "image_url": { // @ts-ignore
+                    "url": BASE64URL+(new Uint8Array(await data?.dataSource?.arrayBuffer())?.toBase64?.({ alphabet: "base64url" })),
+                    "detail": "high"
+                }
+            }
         } else {
             // anyways returns Promise<string>
-            return data?.dataSource?.text();
+            return {
+                "text": data?.dataSource?.text()
+            }
         }
     }
 
     // is not Blob or File, so it's (may be) string (if not string, try to parse it as JSON)
     let result = data?.dataSource;
     try { result = (typeof data?.dataSource != "object") ? data?.dataSource : JSON.stringify(data?.dataSource); } catch (e) { console.warn(e); }
-    return result;
+
+    //
+    return {
+        "text": result
+    }
 }
 
 //
@@ -26,6 +37,7 @@ export class GPTConversion {
     //
     private apiUrl: string = "https://openai.api.proxyapi.ru/v1";//"https://api.openai.com/v1";
     private model: string = "gpt-5";
+    private conversionId?: string|null = null;
 
     //
     protected pending: any[] = [];
@@ -55,17 +67,31 @@ export class GPTConversion {
     //
     async convertPlainToInput(request: (string|Blob|File|any), dataKind: DataKind|null = null): Promise<any> {
         dataKind ??= getDataKindByMIMEType(request?.type) || "text";
-        return { role: "user", type: typesForKind[dataKind], content: await getUsableData({ dataSource: request, dataKind }) };
+        return {
+            type: "message",
+            role: "user",
+            content: [
+                { type: "text", text: "What to do:" },
+                { type: typesForKind[dataKind], ...await getUsableData({ dataSource: request, dataKind }) }
+            ]
+        };
     }
 
     //
     async addToRequest(request: (string|Blob|File|any), dataKind: DataKind|null = null) {
-        this.pending.push(await this.convertPlainToInput(request, dataKind ??= getDataKindByMIMEType(request?.type) || "text"));
+        this.pending.push(await this.convertPlainToInput(request, dataKind ??= getDataKindByMIMEType(request?.type)));
     }
 
     //
     async addInstruction(instruction: string) {
-        this.messages.push({ role: "system", content: instruction });
+        this.pending.push({
+            type: "message",
+            role: "system",
+            content: [{
+                type: "text",
+                text: instruction
+            }]
+        });
     }
 
     //
@@ -75,16 +101,17 @@ export class GPTConversion {
             body: JSON.stringify({
                 model: this.model,
                 tools: this.tools,
-                input: [...this.messages, ...this.pending],
-                reasoning: "medium",
-                temperature: 0.5,
-                response_format: { type: "text" }
+                input: [...this.pending],
+                reasoning: {"effort": "medium"},
+                previous_response_id: this.conversionId
             }),
         });
 
         //
-        this.messages.push(...this.pending); this.pending?.splice(0, this.pending.length);
-        this.messages.push(...((await response.json())?.output || []));
+        const resp = await response.json();
+        this.conversionId = resp?.id;
+        this.messages.push(...(this.pending || [])); this.pending?.splice(0, this.pending.length);
+        this.messages.push(...(resp?.output || []));
         return this.messages[this.messages.length - 1];
     }
 
