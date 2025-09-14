9201e1351b2e0b5b9b7c3fe20502bc5316b318c0
u2re-dev <c24b@u2re.ru>
2025-09-14 09:28:36 +0700

    Toolbar

--- src/frontend/layout/AppLayout.ts
+++ src/frontend/layout/AppLayout.ts
@@ -4,7 +4,13 @@ import { Sidebar } from "./Sidebar";
 
 //
 export const AppLayout = (views: Map<string, HTMLElement>) => {
+    const TOOLBAR = H`<div class="toolbar" slot="bar">
+        <button><ui-icon icon="clipboard"></ui-icon><span>Paste and Recognize</span></button>
+        <button><ui-icon icon="crop"></ui-icon><span>Snip and Recognize</span></button>
+    </div>`;
+
+    //
     const currentView = ref([...views?.keys?.()]?.[0]);
-    const $layout = H`<ui-box-with-sidebar>${[H`<h3 slot="bar">RAS-klad</h3>`, I({ mapped: views, current: currentView }), Sidebar(currentView)]}</ui-box-with-sidebar>`;
+    const $layout = H`<ui-box-with-sidebar>${[TOOLBAR, I({ mapped: views, current: currentView }), Sidebar(currentView)]}</ui-box-with-sidebar>`;
     return $layout;
 }
